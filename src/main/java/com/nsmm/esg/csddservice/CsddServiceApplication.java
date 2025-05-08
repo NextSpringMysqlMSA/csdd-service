7d60901ca5e7667eb42dc8890083695aa6960686
donghwan <kdhe9797@naver.com>
2025-05-08 11:02:59 +0900

    feat: 기본 세팅=

---
+++ src/main/java/com/nsmm/esg/csddservice/CsddServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nsmm.esg.csddservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsddServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsddServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nsmm/esg/csddservice/CsddServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nsmm.esg.csddservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsddServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
