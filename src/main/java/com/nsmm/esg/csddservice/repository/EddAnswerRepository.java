e46cb485ab00482361d78bb5cdfd2b15d36a0e6c
hgg <gksrhkdrn44@naver.com>
2025-05-13 16:11:40 +0900

    feat: csddd 1차 마무리

---
+++ src/main/java/com/nsmm/esg/csddservice/repository/EddAnswerRepository.java
@@ -0,0 +1,18 @@
+package com.nsmm.esg.csddservice.repository;
+
+import com.nsmm.esg.csddservice.entity.EddAnswer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface EddAnswerRepository extends JpaRepository<EddAnswer, Long> {
+
+    // 특정 사용자(memberId)의 모든 응답 조회
+    List<EddAnswer> findAllByMemberId(Long memberId);
+
+    // "아니오(false)"로 응답한 항목만 조회
+    List<EddAnswer> findByMemberIdAndAnswerFalse(Long memberId);
+
+    // 특정 사용자의 모든 응답 삭제
+    void deleteByMemberId(Long memberId);
+}
