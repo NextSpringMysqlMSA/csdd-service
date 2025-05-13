c4d7cd6989153164adef2a43dbd92618a7420b4c
donghwan <kdhe9797@naver.com>
2025-05-13 16:25:36 +0900

    refactor : 레포 수정

--- src/main/java/com/nsmm/esg/csddservice/repository/EddAnswerRepository.java
+++ src/main/java/com/nsmm/esg/csddservice/repository/EddAnswerRepository.java
@@ -7,8 +7,6 @@ import java.util.List;
 
 public interface EddAnswerRepository extends JpaRepository<EddAnswer, Long> {
 
-    // 특정 사용자(memberId)의 모든 응답 조회
-    List<EddAnswer> findAllByMemberId(Long memberId);
 
     // "아니오(false)"로 응답한 항목만 조회
     List<EddAnswer> findByMemberIdAndAnswerFalse(Long memberId);
--- src/main/java/com/nsmm/esg/csddservice/repository/EuddAnswerRepository.java
+++ src/main/java/com/nsmm/esg/csddservice/repository/EuddAnswerRepository.java
@@ -7,8 +7,6 @@ import java.util.List;
 
 public interface EuddAnswerRepository extends JpaRepository<EuddAnswer, Long> {
 
-    // 특정 사용자(memberId)의 모든 응답 조회
-    List<EuddAnswer> findAllByMemberId(Long memberId);
 
     // "아니오(false)"로 응답한 항목만 조회
     List<EuddAnswer> findByMemberIdAndAnswerFalse(Long memberId);
